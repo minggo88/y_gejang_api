a607d4df7ff3a0e4b0471c9990807ffff31372f0
kmg <flyminggo@naver.com>
2024-12-26 13:53:18 +0900

    update

--- v1.0/login/index.php
+++ v1.0/login/index.php
@@ -32,16 +32,6 @@ if(md5($userpw) != $member->userpw) {
     $tradeapi->error('031', __('The information does not match. Please check your ID!'));
 }
 
-// 이메일 인증 확인.
-if($member->bool_confirm_email < 1) {
-    $tradeapi->error('031', __('You did not verify your email. Please check your email verification!'));
-}
-
-// put fcm tokenid + add device
-$_r = $tradeapi->get_fcm_info($member->userno, $uuid);
-if(!$_r) {
-    $tradeapi->put_fcm_info($member->userno, $uuid, $os, $fcm_tokenid);
-}
 
 // login - userno, $userid, $name, $level_code)
 $_r = $tradeapi->login($member->userno, $member->userid, $member->name, $member->level_code);
