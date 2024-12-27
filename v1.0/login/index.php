013ab267459bee22e8e18211d3e5a0661469a7b7
kmg <flyminggo@naver.com>
2024-12-27 16:46:22 +0900

    token 정보삭제제

--- v1.0/login/index.php
+++ v1.0/login/index.php
@@ -2,9 +2,9 @@
 include dirname(__file__) . "/../../lib/TradeApi.php";
 
 // 거래소 api는 토큰을 전달 받을때만 작동하도록 되어 있어서 로그인시 token을 생성해 줍니다.
-$tradeapi->token = session_create_id();
+//$tradeapi->token = session_create_id();
 session_start();
-session_regenerate_id(); // 로그인할때마다 token 값을 바꿉니다. 
+//session_regenerate_id(); // 로그인할때마다 token 값을 바꿉니다. 
 
 // 로그인 세션 확인.
 // $tradeapi->checkLogout();
