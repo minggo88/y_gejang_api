79a00024ba798a59a5b806756821b03ab1492edc
kmg <flyminggo@naver.com>
2024-12-27 19:08:26 +0900

    test11

--- lib/config.php
+++ lib/config.php
@@ -1,16 +1,28 @@
 <?php
+// CORS 설정
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
+// 데이터베이스 연결 설정
 $mysql_hostname = 'dev.cxuwu04we8ge.ap-northeast-2.rds.amazonaws.com';
 $mysql_username = 'admin';
 $mysql_password = 'a2633218*';
 $mysql_database = 'yeosu_clean_gejang';
 
+// 에러 보고 활성화
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-date_default_timezone_set('Asia/Seoul');
-
+// 데이터베이스 연결
 $conn = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, "3306");
+if (!$conn) {
+    http_response_code(500);
+    echo json_encode(["success" => false, "error" => "Database connection failed."]);
+    exit;
+}
 
-
+// 요청 데이터 가져오기
+$request = $_POST;
 ?>
--- v1.0/login/index.php
+++ v1.0/login/index.php
@@ -1,30 +1,8 @@
 <?php
-// CORS 설정 (필요에 따라 수정 가능)
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
 
-// 데이터베이스 연결 설정
-$mysql_hostname = 'dev.cxuwu04we8ge.ap-northeast-2.rds.amazonaws.com';
-$mysql_username = 'admin';
-$mysql_password = 'a2633218*';
-$mysql_database = 'yeosu_clean_gejang';
+// 공통 설정 포함
+include __DIR__ . "/../../lib/config.php";
 
-// 에러 보고 활성화
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-// 데이터베이스 연결
-$conn = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, "3306");
-if (!$conn) {
-    http_response_code(500);
-    echo json_encode(["success" => false, "error" => "Database connection failed."]);
-    exit;
-}
-
-// 요청 데이터 가져오기
-$request = $_POST;
 $social_id = isset($request['social_id']) ? $request['social_id'] : null;
 $userpw = isset($request['userpw']) ? $request['userpw'] : null;
 $os = isset($request['os']) ? $request['os'] : null;
