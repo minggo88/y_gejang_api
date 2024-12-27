34cb0de2f3d4267f66d2e70e4c57bbc8115583e9
kmg <flyminggo@naver.com>
2024-12-27 18:48:41 +0900

    test4

--- v1.0/login/index.php
+++ v1.0/login/index.php
@@ -1,9 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 //include dirname(__file__) . "/../../lib/TradeApi.php";
 
 // 거래소 api는 토큰을 전달 받을때만 작동하도록 되어 있어서 로그인시 token을 생성해 줍니다.
 //$tradeapi->token = session_create_id();
-session_start();
+//session_start();
 //session_regenerate_id(); // 로그인할때마다 token 값을 바꿉니다. 
 
 // 로그인 세션 확인.
