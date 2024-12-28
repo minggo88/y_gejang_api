f0aeed9b0aff8a135a2b5d2baeac5376e86feab8
kmg <flyminggo@naver.com>
2024-12-28 17:27:33 +0900

    deleteItemData make

---
+++ v1.0/deleteItemData/index.php
@@ -0,0 +1,37 @@
+<?php
+// 공통 설정 포함
+include __DIR__ . "/../../lib/config.php";
+
+
+$d_index = setDefault(loadParam('c_index'), '');
+
+
+
+// 가입
+
+$sql = " DELETE FROM `kkikda`.`js_test_item` 
+			WHERE  `item_index`='$d_index';";
+
+
+
+// SQL 실행
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    // 결과 반환
+    echo json_encode([
+        "success" => true,
+        "sql" => $sql
+    ]);
+} else {
+    // SQL 오류 디버깅 메시지
+    echo json_encode([
+        "success" => false,
+        "error" => mysqli_error($conn),
+        "sql" => $sql
+    ]);
+}
+
+// 연결 종료
+mysqli_close($conn);
+?>
